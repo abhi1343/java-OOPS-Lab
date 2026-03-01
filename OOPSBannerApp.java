aecd2194cc164075b640df286e062fe0c79c90ba
abhi1343 <mahataabhinandan7@gmail.com>
2026-03-02 02:57:23 +0530

    complete UC3 stringjoin

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,19 @@
+/**
+ * OOPSBannerApp UC3 - OOPS Banner Application (Use Case 3)
+ * This class improves efficiency by using String.join() instead of the + operator.
+ */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        // Line 1
-        System.out.println("  *****   " + "  *****   " + "  *****   " + "  *****   ");
-
-        // Line 2
-        System.out.println(" **   **  " + " **   **  " + " **   **  " + " **   **  ");
-
-        // Line 3
-        System.out.println(" **   **  " + " **   **  " + " **   **  " + " **       ");
-
-        // Line 4
-        System.out.println(" **   **  " + " **   **  " + "  *****   " + "  *****   ");
-
-        // Line 5
-        System.out.println(" **   **  " + " **   **  " + " **       " + "       ** ");
-
-        // Line 6
-        System.out.println(" **   **  " + " **   **  " + " **       " + " **   **  ");
-
-        // Line 7
-        System.out.println("  *****   " + "  *****   " + " **       " + "  *****   ");
+        // Each line is constructed using String.join(" ", ...) with individual segments
+        // Following the 7-line format with 9-character width per letter
+        
+        System.out.println(String.join(" ", "  *** ", "  *** ", " ***** ", " ***** "));
+        System.out.println(String.join(" ", " ** **", "** **", "** **", "** "));
+        System.out.println(String.join(" ", " ** **", "** **", "** **", "** "));
+        System.out.println(String.join(" ", " ** **", "** **", " ***** ", " ***** "));
+        System.out.println(String.join(" ", " ** **", "** **", "** ", "     **"));
+        System.out.println(String.join(" ", " ** **", "** **", "** ", "     **"));
+        System.out.println(String.join(" ", "  *** ", "  *** ", "** ", " ***** "));
     }
 }
